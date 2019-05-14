bd47964186c2da904dbdb59fc25de8525995c42d
huangqingbing 00442524 <huangqingbing1@huawei.com>
2019-05-14 15:31:01 +0800

    init commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/bingohuang/usegomod
+
+go 1.12
+
+require github.com/bingohuang/testgomod v1.0.0
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/bingohuang/testgomod"
+)
+
+func main() {
+	fmt.Println(testgomod.Hello("bingohuang"))
+}
