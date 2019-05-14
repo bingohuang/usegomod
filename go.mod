46762d452665a1353eadeea22d5ce7c4af16985f
huangqingbing 00442524 <huangqingbing1@huawei.com>
2019-05-14 16:35:21 +0800

    Upgrade gotestmod to v2.0.0

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/bingohuang/usegomod
 
 go 1.12
 
-require github.com/bingohuang/testgomod v1.0.1
+require (
+	github.com/bingohuang/testgomod v1.0.1
+	github.com/bingohuang/testgomod/v2 v2.0.0
+)
--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"fmt"
 
-	"github.com/bingohuang/testgomod"
+	"github.com/bingohuang/testgomod/v2"
 )
 
 func main() {
-	fmt.Println(testgomod.Hello("bingohuang"))
+	hi, err := testgomod.Hello("bingohuang", "cn")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(hi)
 }
